7156ce704090a0071f13d082cbac4411ea2bd782
barosz0 <barosz0@wp.pl>
2023-03-02 18:35:33 +0100

    add Greedy_cycle class and methods

---
+++ lab1vscode/Greedy_cycle.hpp
@@ -0,0 +1,172 @@
+#include "parser.hpp"
+#include <cstdlib>
+#include <list>
+
+class Greedy_cycle
+{
+private:
+    vector<vector<int>> matrix;
+    vector<pair<double, double>> coords; 
+    
+    void choose_random_starting(int &a,int &b);
+    int next_pick(list<int> cycle, vector<bool> taken, pair<int,int> &selected_pair);
+    int get_element(list<int> _list, int index);
+    
+public:
+    int cycle_length;
+    list<int> A_cycle;
+    list<int> B_cycle;
+
+    void run(int a, int b);
+
+    Greedy_cycle(string file);
+    ~Greedy_cycle();
+};
+
+int Greedy_cycle::get_element(list<int> _list, int index)
+{
+    list<int>::iterator it = _list.begin();
+    advance(it,index);
+    return *it;
+}
+
+int Greedy_cycle::next_pick(list<int> cycle, vector<bool> taken, pair<int,int> &selected_pair)
+{
+    pair<int,int> min_p;
+
+    int selected = -1;
+    int min_cost = -1;
+
+    int new_cost;
+    for(int j = 0; j < cycle.size(); j++)
+    {
+        pair<int,int> new_p;
+
+        new_p.first = get_element(cycle, j); //cycle[j];
+
+
+        if(j+1 == cycle.size())
+            new_p.second = get_element(cycle, 0);//cycle[0];
+        else
+            new_p.second = get_element(cycle, j+1);//cycle[j+1]
+        for(int i = 0; i < coords.size(); i++)
+        {
+            if(!taken[i])
+            {
+                if(selected == -1)
+                {
+                    selected = i;
+                    min_cost = matrix[new_p.first][i] + matrix[new_p.second][i] - matrix[new_p.first][new_p.second]; 
+                    min_p = new_p;
+                    continue;
+                }
+
+                new_cost = matrix[new_p.first][i] + matrix[new_p.second][i] - matrix[new_p.first][new_p.second];
+                
+                if(new_cost < min_cost)
+                {
+                    min_p = new_p;
+                    min_cost = new_cost;
+                    selected = i;
+                }
+            }    
+        }
+    }
+    selected_pair = min_p;
+    return selected;
+}
+
+void Greedy_cycle::choose_random_starting(int &a, int &b)
+{
+    srand((unsigned) time(NULL));
+    a = rand() % coords.size();
+
+    b = 0;
+    for(int i = 1; i < coords.size(); i++)
+    {
+        if(matrix[a][b] < matrix[a][i])
+        {
+            b = i;
+        }
+    }
+}
+
+void Greedy_cycle::run(int a, int b)
+{
+
+    cout <<"Zaczynam od a:" << a << " b:" << b << endl;
+    list<int> A{a};
+    int A_length = 0;
+    list<int> B{b};
+    int B_length = 0;
+    
+    vector<bool> taken(coords.size());
+    taken[b] = taken[a] = true;
+
+    pair<int,int> a_place, b_place;
+    int a_cost, b_cost; 
+
+    while(A.size() + B.size() < coords.size())
+    {
+        a = next_pick(A,taken,a_place);
+        a_cost = matrix[a_place.first][a] + matrix[a_place.second][a] - matrix[a_place.first][a_place.second];
+
+        b = next_pick(B,taken,b_place);
+        b_cost = matrix[b_place.first][b] + matrix[b_place.second][b] - matrix[b_place.first][b_place.second];
+
+        taken[a] = true;
+        taken[b] = true;
+
+        if(a==b)
+        {
+            if(a_cost<b_cost)
+            {
+                b = next_pick(B,taken,b_place);
+                b_cost = matrix[b_place.first][b] + matrix[b_place.second][b] - matrix[b_place.first][b_place.second];
+                taken[b] = true;
+            }
+            else
+            {
+                a = next_pick(A,taken,a_place);
+                a_cost = matrix[a_place.first][a] + matrix[a_place.second][a] - matrix[a_place.first][a_place.second];
+                taken[a] = true;
+            }
+        }
+
+        A_length += a_cost;
+        B_length += b_cost;
+
+        list<int>::iterator it = A.begin();
+        advance(it,a_place.second);
+        A.insert(it,a);
+        
+        it = B.begin();
+        advance(it,b_place.second);
+        B.insert(it,b);
+
+        
+
+    }
+
+    cycle_length = A_length + B_length;
+    A_cycle = A;
+    B_cycle = B;
+}
+
+Greedy_cycle::Greedy_cycle(string file)
+{
+    parser p(file);
+
+    matrix = p.matrix;
+    coords = p.coords;
+
+    int a,b;
+
+    choose_random_starting(a,b);
+    run(a,b);
+}
+
+Greedy_cycle::~Greedy_cycle()
+{
+}
+
--- lab1vscode/Lab1.cpp
+++ lab1vscode/Lab1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "parser.hpp"
+#include "Greedy_cycle.hpp"
 
 void print_2d_vector(const std::vector<std::vector<int>>& vec) {
     for (const auto& row : vec) {
@@ -10,10 +11,29 @@ void print_2d_vector(const std::vector<std::vector<int>>& vec) {
     }
 }
 
+void showlist(list<int> g)
+{
+    list<int>::iterator it;
+    for (it = g.begin(); it != g.end(); ++it)
+        cout << '\t' << *it;
+    cout << '\n';
+}
+
 int main()
 {
-    parser p("Data/kroA100.tsp");
+    // parser p("Data/kroA100.tsp");
     // parser p("Data/kroB100.tsp");
+    cout << "---------------start---------------" << endl;
+    Greedy_cycle cycle("Data/kroB100.tsp");
+    cout << "---------------end---------------" << endl;
+    cout << "Dlugosc :" << cycle.cycle_length << endl;
+
+    cout << "Cykl A:" << endl;
+    showlist(cycle.A_cycle);
+
+    cout << "Cykl B:" << endl;
+    showlist(cycle.B_cycle);
+
 
     // print_2d_vector(p.matrix);
 
