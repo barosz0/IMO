8fb8b0889f6548ec1ba6790b579f2933d1f6d4d4
barosz0 <barosz0@wp.pl>
2023-03-10 02:09:03 +0100

    update main

--- lab1vscode/Lab1.cpp
+++ lab1vscode/Lab1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "parser.hpp"
 #include "Greedy_cycle.hpp"
+#include "Greedy_2regret_cycle.hpp"
 
 void print_2d_vector(const std::vector<std::vector<int>>& vec) {
     for (const auto& row : vec) {
@@ -19,20 +20,48 @@ void showlist(list<int> g)
     cout << '\n';
 }
 
-int main()
+void greedy_cycle_run()
 {
-    // parser p("Data/kroA100.tsp");
-    // parser p("Data/kroB100.tsp");
+    cout << "Greedy cycle Algorithm" << endl;
     cout << "---------------start---------------" << endl;
     Greedy_cycle cycle("Data/kroB100.tsp");
     cout << "---------------end---------------" << endl;
-    cout << "Dlugosc :" << cycle.cycle_length << endl;
 
     cout << "Cykl A:" << endl;
     showlist(cycle.A_cycle);
+    cout << "Dlugosc :" << cycle.A_length << endl;
 
     cout << "Cykl B:" << endl;
     showlist(cycle.B_cycle);
+    cout << "Dlugosc :" << cycle.B_length << endl;
+}
+
+void greedy_2regret_cycle_run()
+{
+    cout << "Greedy 2regret cycle Algorithm" << endl;
+    cout << "---------------start---------------" << endl;
+    Greedy_2regret_cycle cycle("Data/kroB100.tsp");
+    cout << "---------------end---------------" << endl;
+
+    cout << "Cykl A:" << endl;
+    showlist(cycle.A_cycle);
+    cout << "Dlugosc :" << cycle.A_length << endl;
+
+    cout << "Cykl B:" << endl;
+    showlist(cycle.B_cycle);
+    cout << "Dlugosc :" << cycle.B_length << endl;
+}
+
+int main()
+{
+
+    // parser p("Data/kroA100.tsp");
+    // parser p("Data/kroB100.tsp");
+
+    greedy_cycle_run();
+    
+    greedy_2regret_cycle_run();
+    
 
 
     // print_2d_vector(p.matrix);
