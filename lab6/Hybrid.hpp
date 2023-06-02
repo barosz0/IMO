2199c9686e575acfbe1dace4f9db8a8556da3d0a
barosz0 <barosz0@wp.pl>
2023-06-02 08:12:40 +0200

    add lab6

---
+++ lab6/Hybrid.hpp
@@ -0,0 +1,288 @@
+#ifndef HYBRID_EVOLUTIONARY
+#define HYBRID_EVOLUTIONARY
+
+#include "Local_search_abstract.hpp"
+#include "Greedy_2regret_cycle.hpp"
+#include "Memory_search.hpp"
+#include "Random_cycle.hpp"
+#include <random>
+
+struct solution
+{
+    std::vector<int> A_cycle;
+    std::vector<int> B_cycle;
+    int score;
+
+    solution(Local_search_abstract *LS)
+    {
+        A_cycle = LS->get_A_cycle();
+        B_cycle = LS->get_B_cycle();
+
+        score = LS->get_length();
+    }
+
+    solution(Cycle_abstract *cycle)
+    {
+        A_cycle = vector<int>(cycle->A_cycle.size());
+        std::copy(cycle->A_cycle.begin(), cycle->A_cycle.end(), A_cycle.begin());
+
+        B_cycle = vector<int>(cycle->B_cycle.size());
+        std::copy(cycle->B_cycle.begin(), cycle->B_cycle.end(), B_cycle.begin());
+
+        score = cycle -> get_length();
+    }
+    solution()
+    {
+        score = -1;
+    }
+};
+
+
+class Hybrid : public Local_search_abstract
+{
+private:
+    std::random_device r;
+    std::default_random_engine rng = std::default_random_engine(r());
+
+    std::vector<solution> population;
+    void fill_starting_population(int n);
+    bool check_if_score_in_population(int score);
+    bool check_if_edge_in_cycle(int a, int b, vector<int> &cycle);
+    int check_if_enter_population(solution sol);
+    solution make_new_solution();
+    solution combine_solutions(solution A, solution B);
+
+    solution find_best_solution();
+
+    solution best_solution;
+    decltype(std::chrono::high_resolution_clock::now()) start_time;
+
+    int population_size;
+    int max_running_time;
+    bool do_local_search;
+    string filename;
+    
+    void run() override;
+public:
+    Hybrid(string filename_, int max_running_time_, int population_size_);
+    ~Hybrid();
+};
+
+solution Hybrid::find_best_solution()
+{
+    solution ret = population[0];
+
+    for(int i=0; i<population.size();i++)
+    {
+        if(ret.score > population[i].score)
+        {
+            ret = population[i];
+        }
+    }
+    return ret;
+}
+
+bool Hybrid::check_if_edge_in_cycle(int a, int b, vector<int> &cycle)
+{
+    for(int i = 0; i < cycle.size()-1; i++)
+    {
+        if(cycle[i] == a)
+            if(cycle[i+1] == b)
+                return true;
+    }
+    if(cycle[cycle.size()-1] == a)
+        if(cycle[0] == b)
+            return true;
+    
+    return false;
+}
+
+int Hybrid::check_if_enter_population(solution sol)
+{
+    int position = -1;
+    for(int i=0; i<population.size();i++)
+    {
+        if(population[i].score == sol.score) return -1;
+        if(population[i].score > sol.score)
+        {
+            if(position == -1)
+                position = i;
+            else if(population[i].score < population[position].score)
+                position = i;
+
+        }
+    }
+    return position;
+}
+
+
+solution Hybrid::combine_solutions(solution A, solution B)
+{
+    // printf("combine solution\n");
+    vector<int> new_A_cycle;
+    bool second_push = false;
+    for(int i = 0; i < A.A_cycle.size()-1; i++)
+    {
+        if(check_if_edge_in_cycle(A.A_cycle[i],A.A_cycle[i+1], B.A_cycle))
+        {
+            new_A_cycle.push_back(A.A_cycle[i]);
+            second_push = true;
+        }
+        else if(second_push)
+        {
+            second_push = false;
+            new_A_cycle.push_back(A.A_cycle[i]);
+        }
+    }
+
+    if(check_if_edge_in_cycle(A.A_cycle[A.A_cycle.size()-1],A.A_cycle[0], B.A_cycle))
+    {
+        new_A_cycle.push_back(A.A_cycle[A.A_cycle.size()-1]);
+        second_push = true;
+        if(new_A_cycle[0] != A.A_cycle[0])
+            new_A_cycle.push_back(A.A_cycle[0]);
+    }
+    else if(second_push)
+    {
+        second_push = false;
+        new_A_cycle.push_back(A.A_cycle[A.A_cycle.size()-1]);
+    }
+    // printf("first half\n");
+    vector<int> new_B_cycle;
+    second_push = false;
+    for(int i = 0; i < A.B_cycle.size()-1; i++)
+    {
+        if(check_if_edge_in_cycle(A.B_cycle[i],A.B_cycle[i+1], B.B_cycle))
+        {
+            new_B_cycle.push_back(A.B_cycle[i]);
+            second_push = true;
+        }
+        else if(second_push)
+        {
+            new_B_cycle.push_back(A.B_cycle[i]);
+        }
+    }
+
+    if(check_if_edge_in_cycle(A.B_cycle[A.B_cycle.size()-1],A.B_cycle[0], B.B_cycle))
+    {
+        new_B_cycle.push_back(A.B_cycle[A.B_cycle.size()-1]);
+        second_push = true;
+        if(new_B_cycle[0] != A.B_cycle[0])
+            new_B_cycle.push_back(A.B_cycle[0]);
+    }
+    else if(second_push)
+    {
+        second_push = false;
+        new_B_cycle.push_back(A.B_cycle[A.B_cycle.size()-1]);
+    }
+    // printf("Making cycle %d and %d\n", new_A_cycle.size(), new_B_cycle.size());
+    // if(new_A_cycle.size() == 0 || new_B_cycle.size() == 0)
+    //     printf("<++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+    Greedy_2regret_cycle cycle = Greedy_2regret_cycle(matrix,coords, new_A_cycle,new_B_cycle);
+
+    if(do_local_search){
+        Memory_search MS = Memory_search(&cycle);
+        return solution(&MS);
+    }
+    // printf("Combine end\n");
+
+    return solution(&cycle);
+}
+
+solution Hybrid::make_new_solution()
+{
+    std::uniform_int_distribution<int> myUnifIntDist(0,population.size()-1);
+    int a = myUnifIntDist(rng);
+    int b = myUnifIntDist(rng);
+    while (a==b) b = myUnifIntDist(rng);
+
+    solution A = population[a], B = population[b];
+    return combine_solutions(A,B);
+
+}
+
+void Hybrid::run()
+{
+    // printf("run\n");
+    // auto start = std::chrono::high_resolution_clock::now();
+    auto start = start_time;
+
+    while(true)
+    {
+        moves++;
+        solution s = make_new_solution();
+        int index = check_if_enter_population(s);
+        // printf("index: %d\n", index);
+        if(index != -1)
+        {
+            // printf("Nowy czlonek spoleczenstwa (score = %d)\n", s.score);
+            population[index] = s;
+        }
+
+        auto end = std::chrono::high_resolution_clock::now(); // pobranie aktualnego czasu
+        auto elapsed_time = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count(); // obliczenie czasu jaki minął od startu
+        if (elapsed_time >= max_running_time) // sprawdzenie, czy czas trwania został osiągnięty
+            {
+                break;
+            }
+        // printf("while end;\n");
+    }
+}
+
+
+void Hybrid::fill_starting_population(int n)
+{
+    while(population.size() < n)
+    {   
+        Random_cycle cycle = Random_cycle(filename);
+        Memory_search LS = Memory_search(&cycle);   
+        if(!check_if_score_in_population(LS.get_length()))
+        {
+            population.push_back(solution(&LS));
+        }
+
+    }
+}
+
+bool Hybrid::check_if_score_in_population(int score)
+{
+    for(int i = 0; i<population.size(); i++)
+    {
+        if(population[i].score == score)
+            return true;
+    }
+    return false;
+}
+
+Hybrid::Hybrid(string filename_, int max_running_time_, int population_size_ = 20)
+{
+    population_size = population_size_;
+    filename = filename_;
+    max_running_time = max_running_time_;
+
+    parser p(filename_);
+    matrix = p.matrix;
+    coords = p.coords;
+
+    filename = filename_;
+
+    start_time = std::chrono::high_resolution_clock::now();
+
+    fill_starting_population(population_size);
+    best_solution = find_best_solution();
+    // printf("Startowa najlepsza: %d\n",best_solution.score);
+    run();
+    best_solution = find_best_solution();
+    // printf("Koncowa najlepsza: %d\n",best_solution.score);
+    A_cycle = best_solution.A_cycle;
+    B_cycle = best_solution.B_cycle;
+    recalculate_length();
+}
+
+
+Hybrid::~Hybrid()
+{
+    
+}
+
+#endif
---
+++ lab6/Local_search_abstract.hpp
@@ -0,0 +1,169 @@
+#ifndef LS_ABSTRACT
+#define LS_ABSTRACT
+
+#include "Cycle_abstract.hpp"
+
+
+class Local_search_abstract
+{
+protected:
+    /* data */
+    vector<vector<int>> matrix;
+    vector<pair<double, double>> coords;
+    
+    Cycle_abstract* orginal_cycle;
+    std::vector<int> A_cycle;
+    std::vector<int> B_cycle;
+    int A_length;
+    int B_length;
+    int moves;
+    pair<int,int> get_adjacent_vertex(vector<int> v, int index);
+
+    void recalculate_length();
+    virtual void run() = 0;
+    
+public:
+    int get_length();
+    vector<pair<double, double>> get_cycle_coords(vector<int> cycle);
+
+    vector<pair<double, double>> get_A_coords();
+    vector<pair<double, double>> get_B_coords();
+    int get_moves();
+
+    std::vector<int> get_A_cycle();
+    std::vector<int> get_B_cycle();
+    
+    Local_search_abstract(Cycle_abstract *cycle);
+    Local_search_abstract();
+    ~Local_search_abstract();
+    
+};
+
+int Local_search_abstract::get_moves()
+{
+    return moves;
+}
+
+std::vector<int> Local_search_abstract::get_A_cycle()
+{
+    return A_cycle;
+}
+
+std::vector<int> Local_search_abstract::get_B_cycle()
+{
+    return B_cycle;
+}
+
+pair<int,int> Local_search_abstract::get_adjacent_vertex(vector<int> v, int index)
+{
+    pair<int,int> ret;
+
+    if(index==0)
+    {
+        ret.first = v.back();
+        ret.second = v[index+1];
+    }
+    else if (index==v.size()-1)
+    {
+        ret.first = v[index-1];
+        ret.second = v[0];
+    }
+    else
+    {
+        ret.first = v[index-1];
+        ret.second = v[index+1];
+    }
+
+    return ret;
+}
+
+vector<pair<double, double>> Local_search_abstract::get_A_coords()
+{
+    return get_cycle_coords(A_cycle);
+}
+
+vector<pair<double, double>> Local_search_abstract::get_B_coords()
+{
+    return get_cycle_coords(B_cycle);
+}
+
+vector<pair<double, double>> Local_search_abstract::get_cycle_coords(vector<int> cycle)
+{
+    vector<pair<double, double>> cycle_coords;
+    vector<pair<double, double>> coords = orginal_cycle->get_coords();
+    
+    for (int i = 0; i < cycle.size(); i++)
+        cycle_coords.push_back(coords[cycle[i]]);
+
+    return (cycle_coords);
+}
+
+void Local_search_abstract::recalculate_length()
+{
+    if(A_cycle.size() > 0)
+    {    
+        int len = 0;
+        for(int a = 0; a < A_cycle.size()-1; a++)
+        {
+            // printf("a: %d\n",a);
+            len += matrix[A_cycle[a]][A_cycle[a+1]];
+        }
+        len += matrix[A_cycle.back()][A_cycle[0]];
+        A_length = len;
+    }
+    else
+    {
+        A_length = 0;
+    }
+
+    if(B_cycle.size() > 0)
+    {    
+        int  len = 0;
+        for(int a = 0; a < B_cycle.size()-1; a++)
+        {
+            // printf("b: %d\n",a);
+            len += matrix[B_cycle[a]][B_cycle[a+1]];
+        }
+        len += matrix[B_cycle.back()][B_cycle[0]];
+        B_length = len;
+    }
+    else
+    {
+        B_length = 0;
+    }
+}
+
+int Local_search_abstract::get_length()
+{
+    recalculate_length();
+    return A_length + B_length;
+}
+
+Local_search_abstract::Local_search_abstract(Cycle_abstract *cycle)
+{
+    
+    orginal_cycle = cycle;
+    A_cycle = vector<int>(cycle->A_cycle.size());
+    // A_cycle = cycle->A_cycle;
+    std::copy(cycle->A_cycle.begin(), cycle->A_cycle.end(), A_cycle.begin());
+    
+    B_cycle = vector<int>(cycle->B_cycle.size());
+    // B_cycle = cycle->B_cycle;
+    std::copy(cycle->B_cycle.begin(), cycle->B_cycle.end(), B_cycle.begin());
+
+    A_length = cycle->A_length;
+    B_length = cycle->B_length;
+
+    matrix = cycle -> get_matrix();
+    moves = 0;
+}
+
+Local_search_abstract::Local_search_abstract(){
+    moves = 0;
+}
+
+Local_search_abstract::~Local_search_abstract()
+{
+}
+
+#endif
